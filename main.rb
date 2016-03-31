a7e6f09d50229f8ca6c5770c6179956a3fbfc414
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-31 14:09:32 -0500

    Required book, library, patron, and staff member in Main

--- main.rb
+++ main.rb
@@ -2,5 +2,9 @@ require_relative "config/environment.rb"
 require "active_record"
 require "pry"
 require "yaml"
+require_relative "lib/book.rb"
+require_relative "lib/library.rb"
+require_relative "lib/patron.rb"
+require_relative "lib/staff_member.rb"
 
 binding.pry

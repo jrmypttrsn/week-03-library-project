bba1f200bd777a0880e2fe4befcad9b9a4cb2df4
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-31 14:47:13 -0500

    Updated validations in library

--- lib/library.rb
+++ lib/library.rb
@@ -6,8 +6,11 @@
 class Library < ActiveRecord::Base
   
   validates :branch_name, presence: true
+  validates :branch_name, uniqueness: true
   validates :address, presence: true
-  validates :phone_number, numericality: { only_integer: true }
+  validates :address, uniqueness: true
+  validates :phone_number, presence: true
+  validates :phone_number, uniqueness: true
 
   has_many :staff_members
   has_many :books
